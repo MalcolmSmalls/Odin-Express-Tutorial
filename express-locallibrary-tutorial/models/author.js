14d3e760412c058e5bcb0ecbba557058effa6ef7
MalcolmSmalls <smallsbeats@gmail.com>
2022-12-19 12:18:52 -0800

    Add test, fix errors stopping test from running all the way through because of typos

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -1,6 +1,6 @@
-const moongose = require ('mongoose')
+const mongoose = require ('mongoose');
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 
 const AuthorSchema = new Schema ({
--- express-locallibrary-tutorial/models/book.js
+++ express-locallibrary-tutorial/models/book.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
+const BookSchema = new Schema({
 	title: { type: String, required: true },
-	author: { type: Schema.Types.Objectid, ref: "Author", required: true},
+	author: { type: Schema.Types.ObjectId, ref: "Author", required: true},
 	summary: { type: String, required: true},
 	isbn: { type: String, required: true},
 	genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }]
@@ -13,7 +13,7 @@ const bookSchema = new Schema({
 });
 
 BookSchema.virtual('url').get(function () {
-	return `/catalog/book/${this.__id}`;
+	return `/catalog/book/${this._id}`;
 });
 
 module.exports = mongoose.model("Book", BookSchema)
